894851f26117bdeadeadbc46bb29bf7f37bad06e
SkymanSky <gokera@ogr.iu.edu.tr>
2021-03-24 00:07:05 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        string MusteriAdi;
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " isminde bir musteri eklendi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Id: " + musteri.MusteriId);
+            Console.WriteLine("Musteri Adı: " + musteri.MusteriAdi);
+            Console.WriteLine("Musteri Soyadı: " + musteri.MusteriSoyadi);
+            Console.WriteLine("Musteri İkameti: " + musteri.MusteriIkamet);
+                             
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " isminde bir musteri silindi.");
+
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ClassMetotDemo
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdi = "Skyman";
+            musteri1.MusteriSoyadi = "Sky";
+            musteri1.MusteriIkamet = "İstanbul";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+
+            Console.WriteLine("------Musteri Listesi------");
+            musteriManager.Listele(musteri1);
+
+            Console.WriteLine("---------------------------");
+            musteriManager.Sil(musteri1);
+
+
         }
     }
 }
